c8f7e2482587d922380e94bb5c81381f0dc8c15d
dd31 <52243140+devi31@users.noreply.github.com>
2023-07-26 16:32:16 +0530

    Update appDepBridge.js

--- deviNFT/scripts/appDepBridge.js
+++ deviNFT/scripts/appDepBridge.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 const tokenJSON = require("../artifacts/contracts/DeviNFT.sol/DeviNFT.json");
-const tokenAddress = "0xcAd0D6B3101979616299a7Db41bEa13a81Aa676fF"; // Extract token address from .env
+const tokenAddress = "token address"; // Extract token address from .env
 const tokenABI = tokenJSON.abi;
-const walletAddress = "0xbEEF8135D7B534390a2EF7C388bDd855CAdF1CA5";
+const walletAddress = "wallet address";
 
 const fxRootContractABI = require("../fxRootContractABI.json");
 const fxERC21RootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
